fbcea7f4ca21b25624a41160b5a1e36ef1039f98
Zhaal <50073058+Zhaal@users.noreply.github.com>
2025-08-09 16:51:05 +0200

    Update ping.js

--- netlify/functions/ping.js
+++ netlify/functions/ping.js
@@ -1,3 +1,12 @@
-exports.handler = async () => {
-  return { statusCode: 200, body: JSON.stringify({ ok: true, node: process.version }) };
-};
+exports.handler = async () => ({
+  statusCode: 200,
+  headers: { 'Content-Type':'application/json' },
+  body: JSON.stringify({
+    ok: true,
+    node: process.version,
+    env: {
+      BLOBS_SITE_ID: !!process.env.BLOBS_SITE_ID,
+      BLOBS_TOKEN:  !!process.env.BLOBS_TOKEN
+    }
+  })
+});
